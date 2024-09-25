151e94a09cea4f0a302e473a37bc77907f1201d3
Odile078 <ouwimpuhwe620@daviscollege.com>
2024-09-25 17:39:02 +0200

    refactor: import using alias @

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import * as SplashScreen from 'expo-splash-screen'
 import 'react-native-reanimated'
-import AppNavigator from './navigation'
-import { AuthProvider } from './contexts/AuthContext'
+import AppNavigator from "@/navigation";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StatusBar } from 'expo-status-bar'
-import { useFont } from "./hooks/useFont";
+import { useFont } from "@/hooks/useFont";
 import { useEffect } from "react";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -15,15 +15,15 @@ const queryClient = new QueryClient();
 export default function App() {
   const { loaded, error } = useFont();
 
+  if (!loaded && !error) {
+    return null;
+  }
+
   useEffect(() => {
     if (loaded || error) {
       SplashScreen.hideAsync();
     }
   }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
 
   return (
     <QueryClientProvider client={queryClient}>
