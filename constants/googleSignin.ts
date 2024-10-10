db775d2273fbf5c0ee53f611bdbbcdafe75050f9
igorntwari <igorntwari28@gmail.com>
2024-10-10 09:55:23 +0200

    refactor: fix env files

--- constants/googleSignin.ts
+++ constants/googleSignin.ts
@@ -1,8 +1,9 @@
 import { GoogleSignin, isErrorWithCode, isSuccessResponse, statusCodes } from "@react-native-google-signin/google-signin";
 import Constants from 'expo-constants';
-const { IOS_CLIENT_ID, WEB_CLIENT_ID } = Constants.extra;
-
-GoogleSignin.configure({
+ const { IOS_CLIENT_ID, WEB_CLIENT_ID } = Constants.expoConfig?.extra || {}
+ console.log('IOS_CLIENT_ID:', IOS_CLIENT_ID);
+ console.log('WEB_CLIENT_ID:', WEB_CLIENT_ID);
+ GoogleSignin.configure({
   iosClientId: IOS_CLIENT_ID,
   webClientId: WEB_CLIENT_ID,
   offlineAccess: true
@@ -13,13 +14,13 @@ export const signInWithGoogle = async () => {
     await GoogleSignin.hasPlayServices();
     const response = await GoogleSignin.signIn();
     if (isSuccessResponse(response)) {
-      return response
+      return response;
     } else {
       console.log('Sign in was cancelled by user');
     }
   } catch (error) {
     if (isErrorWithCode(error)) {
-        console.log(error,"testingggg")
+      console.log(error, "testingggg");
       switch (error.code) {
         case statusCodes.IN_PROGRESS:
           console.log('Sign-in already in progress');
