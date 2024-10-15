6185db29356d8008cd5e159e2932665bc21e4c39
mhenriette <mhenriette00@gmail.com>
2024-10-15 12:54:51 +0200

    refactor: order colors in theme file

--- theme.ts
+++ theme.ts
@@ -1,11 +1,10 @@
 export const theme = {
   colors: {
     black: "#000000",
+    black_300: "#00000033",
+    black_400: "#00000040",
     white: "#FFFFFF",
     white_300: "#FFFFFF33",
-    black_400: "#00000040",
-    black_300: "#00000033",
-
     gray: "#E3E3E3",
     "light-gray": "#898989",
     red: "#6B2222",
