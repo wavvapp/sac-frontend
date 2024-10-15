a576c998dfa47e7c54997fcd3a3da7c56f9b23f6
igorntwari <igorntwari28@gmail.com>
2024-10-15 12:07:51 +0200

    refactor: fix the failing env

--- screens/Authentication/Login/index.tsx
+++ screens/Authentication/Login/index.tsx
@@ -4,17 +4,14 @@ import { useNavigation } from "@react-navigation/native"
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import React, { useState } from "react"
 import { View, TextInput, Button, StyleSheet, Text } from "react-native"
-
 import {
   GoogleSignin,
   GoogleSigninButton,
 } from "@react-native-google-signin/google-signin"
 
 GoogleSignin.configure({
-  webClientId:
-    "351310982043-d2sbe099g0aaq28ussbjfn2l51862t6i.apps.googleusercontent.com",
-  iosClientId:
-    "351310982043-28dk2k254o76heo1d4pr3uftmgsjmihh.apps.googleusercontent.com",
+  webClientId: process.env.WEB_CLIENT_ID,
+  iosClientId: process.env.IOS_CLIENT_ID,
   offlineAccess: true,
 })
 type LoginProp = NativeStackNavigationProp<RootStackParamList, "Login">
