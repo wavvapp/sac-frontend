3564894e9e2ade360da28b1471e0c70914e86803
serapieTuyishime <tuyiserapie1@gmail.com>
2024-10-16 18:14:53 +0200

    feat: update signaling list

--- components/lists/Signaling.tsx
+++ components/lists/Signaling.tsx
@@ -1,5 +1,5 @@
 import React, { forwardRef } from "react"
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, SectionList } from "react-native"
 import CustomText from "@/components/ui/CustomText"
 import UserAvatar from "@/components/ui/UserAvatar"
 import UserInfo from "@/components/UserInfo"
@@ -9,7 +9,6 @@ import { User } from "@/types"
 import { FlatList } from "react-native-gesture-handler"
 import BottomDrawer from "@/components/BottomDrawer"
 import { CustomButton } from "@/components/ui/Button"
-import { theme } from "@/theme"
 export interface SignalingRef {
   openBottomSheet: () => void
 }
@@ -22,75 +21,89 @@ const Signaling = forwardRef<SignalingRef, SignalingProps>((props, ref) => {
   const availableUsers = props.users?.length ? props.users : onlineUsers
   const otherusers = props.users?.length ? props.users : offlineUsers
 
+  const sections: {
+    title: string
+    data: User[]
+    renderItem: ({ item }: { item: User }) => JSX.Element
+  }[] = [
+    {
+      title: "Products",
+      data: availableUsers,
+      renderItem: ({ item }: { item: User }) => (
+        <View style={styles.userCard}>
+          <UserAvatar imageUrl={item.imageUrl || 0} />
+          <View>
+            <UserInfo
+              firstName={item.firstName}
+              lastName={item.lastName}
+              time={item.time}
+              activity={item.activity}
+            />
+          </View>
+        </View>
+      ),
+    },
+    {
+      title: "Supervisors",
+      data: otherusers,
+      renderItem: ({ item }: { item: User }) => (
+        <View style={styles.userCard}>
+          <UserAvatar imageUrl={item.imageUrl || 0} />
+          <View>
+            <UserInfo
+              firstName={item.firstName}
+              lastName={item.lastName}
+              time={item.time}
+              activity={item.activity}
+            />
+          </View>
+        </View>
+      ),
+    },
+  ]
+
   return (
     <BottomDrawer ref={ref}>
-      <View style={styles.container}>
-        <View style={styles.header}>
-          <CustomText size="xl" fontWeight="bold" style={styles.headerText}>
-            Friends
-          </CustomText>
-          {/* TODO: this should redirect to the search screen */}
-          <CustomButton
-            variant="primary"
-            textSize="sm"
-            title="FIND"
-            textStyles={{ fontWeight: 600 }}
-          />
-        </View>
-        <FlatList
-          contentContainerStyle={styles.availableUsers}
-          style={styles.flalist}
-          data={availableUsers as User[]}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.userCard}>
-              <UserAvatar imageUrl={item.imageUrl || 0} />
-              <View>
-                <UserInfo
-                  firstName={item.firstName}
-                  lastName={item.lastName}
-                  time={item.time}
-                  activity={item.activity}
-                />
-              </View>
-            </View>
-          )}
-        />
-        <FlatList
-          contentContainerStyle={styles.otherUsers}
-          style={styles.flalist}
-          data={otherusers as User[]}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <View style={styles.userCard}>
-              <UserAvatar imageUrl={item.imageUrl || 0} />
-              <View>
-                <UserInfo
-                  firstName={item.firstName}
-                  lastName={item.lastName}
-                  time={item.time}
-                  activity={item.activity}
-                />
-              </View>
-            </View>
-          )}
+      <View style={styles.header}>
+        <CustomText size="xl" fontWeight="bold" style={styles.headerText}>
+          Friends
+        </CustomText>
+        {/* TODO: this should redirect to the search screen */}
+        <CustomButton
+          variant="primary"
+          textSize="sm"
+          title="FIND"
+          textStyles={{ fontWeight: 600 }}
         />
       </View>
+      {!availableUsers.length && (
+        <CustomText style={styles.noUsers}>None of your friends on Wavv are available today</CustomText>
+      )}
+      <FlatList
+        data={[1]}
+        keyExtractor={(index) => index.toString()}
+        renderItem={() => (
+          <SectionList
+            sections={sections}
+            keyExtractor={(index) => index.toString()}
+          />
+        )}
+      />
     </BottomDrawer>
   )
 })
 
 const styles = StyleSheet.create({
-  container: {
-    // flex: 1,
-    paddingTop: 16,
-    paddingHorizontal: 20,
-  },
+  // container: {
+  //   flex: 1,
+  //   paddingTop: 16,
+  // },
   header: {
     flexDirection: "row",
     alignItems: "center",
     marginBottom: 20,
     justifyContent: "space-between",
+    paddingHorizontal: 20,
   },
   headerText: {
     fontFamily: "suisse",
@@ -102,20 +115,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     gap: 10,
     paddingBottom: 18,
+    paddingHorizontal: 20,
   },
-  flalist: {
-    width: "100%",
-  },
-  availableUsers: {
-    flexGrow: 1,
-    justifyContent: "flex-start",
-    paddingBottom: 70,
-  },
-  otherUsers: {
-    flexGrow: 1,
-    justifyContent: "flex-start",
-    paddingBottom: 70,
-    backgroundColor: theme.colors.white_100,
+  // flalist: {
+  //   width: "100%",
+  // },
+  // availableUsers: {
+  //   flexGrow: 1,
+  //   justifyContent: "flex-start",
+  //   paddingBottom: 70,
+  // },
+  // avalableUserCard: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   gap: 10,
+  //   backgroundColor: theme.colors.white_100,
+  //   paddingHorizontal: 20,
+  //   paddingVertical: 20,
+  // },
+  noUsers: {
+    padding: 20,
   },
 })
 
