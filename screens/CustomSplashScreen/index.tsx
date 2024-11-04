97642d3e1225e5aad0ea30d873125ef9af9ba72a
igorntwari <igorntwari28@gmail.com>
2024-11-04 10:10:03 +0200

    refactor: uncomment prelinnoise

--- screens/CustomSplashScreen/index.tsx
+++ screens/CustomSplashScreen/index.tsx
@@ -1,16 +1,17 @@
-// import PerlinNoise from "@/components/PerlinNoise"
+import PerlinNoise from "@/components/PerlinNoise"
 import CustomText from "@/components/ui/CustomText"
 import WavvLogo from "@/components/vectors/WavvLogo"
 import { theme } from "@/theme"
 import { StyleSheet, View } from "react-native"
-// import { useSharedValue } from "react-native-reanimated"
+import { useSharedValue } from "react-native-reanimated"
 
 export default function CustomSplashScreen() {
-  // const isOn = useSharedValue(false)
+  const isOn = useSharedValue(false)
 
   return (
     <View style={styles.container}>
-      {/* <PerlinNoise isOn={isOn} color1="#281713" color2="blue" /> */}
+      <PerlinNoise isOn={isOn} color1="#281713" color2="blue" />
+
       <View style={styles.content}>
         <WavvLogo width={390} height={80} />
         <CustomText size="sm" fontFamily="marfa" style={styles.subText}>
