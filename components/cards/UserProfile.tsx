0cf6480e73a9e21cef1ccf325ea4896f2e57eef8
mhenriette <mhenriette00@gmail.com>
2025-01-14 13:33:18 +0200

    feat: create user profile component

---
+++ components/cards/UserProfile.tsx
@@ -0,0 +1,37 @@
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import UserAvatar from "../ui/UserAvatar"
+import { useAuth } from "@/contexts/AuthContext"
+import UserInfo from "@/components/UserInfo"
+
+export default function UserProfile() {
+  const { user } = useAuth()
+  const { names, username } = user || {}
+  return (
+    <View style={styles.container}>
+      <UserAvatar
+        imageUrl={user?.profilePictureUrl}
+        size="medium"
+        style={{ alignSelf: "center" }}
+      />
+      {names && username && (
+        <UserInfo
+          fullName={names}
+          username={username}
+          style={styles.userInfoStyles}
+          size="lg"
+        />
+      )}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    rowGap: 24,
+    alignItems: "center",
+  },
+  userInfoStyles: {
+    alignItems: "center",
+  },
+})
