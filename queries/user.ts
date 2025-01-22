616be39f6077ef568777624d58a1cc4927f93d7b
igorntwari <igorntwari28@gmail.com>
2025-01-22 14:55:08 +0200

    refactor: simplify the logic

--- queries/user.ts
+++ queries/user.ts
@@ -1,23 +1,12 @@
 import api from "@/service"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 
 export const useDeleteUser = () => {
-  const queryClient = useQueryClient()
-
   return useMutation({
     mutationFn: async () => {
       const response = await api.delete("/users")
       return response.data.isDeleted
     },
-
-    onSuccess: async (isDeleted) => {
-      if (isDeleted) {
-        await AsyncStorage.removeItem("@Auth:user")
-        queryClient.clear()
-      }
-    },
-
     onError: (error) => {
       console.error("Error deleting user:", error)
     },
