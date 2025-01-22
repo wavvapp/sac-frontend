2f50e7f290f99b5176b02fec0fac40b5ccb4f4b7
Odile078 <ouwimpuhwe620@daviscollege.com>
2025-01-22 14:12:01 +0200

    reafactor: remove validation TODO

--- constants/patterns.ts
+++ constants/patterns.ts
@@ -1,8 +1,5 @@
 export const VALIDATION_PATTERNS = {
   username: /^[a-zA-Z0-9_\.]{3,16}$/,
   email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-  // The fullname exclude special characters and numbers or extra spaces
-  // Matches any alphabets from any language (including ß, Ä, Ö)
-  // Is well within the 1-50 character limit
-  fullName: /^(?=.{1,50}$)[\p{L}]+(?: [\p{L}]+)*$/u,
+  fullName: /^[a-zA-Z ]{1,50}$/,
 }
