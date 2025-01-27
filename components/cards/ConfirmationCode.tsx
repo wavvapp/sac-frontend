b1aba4117b69fea42e0b2ff75346ee36e3b6545f
mhenriette <mhenriette00@gmail.com>
2025-01-27 11:45:34 +0200

    feat: add verification fields and update app version

---
+++ components/cards/ConfirmationCode.tsx
@@ -0,0 +1,66 @@
+import { theme } from "@/theme"
+import { StyleSheet, TextInput, View } from "react-native"
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field"
+
+const CELL_COUNT = 6
+
+export const ConfirmationCode = ({
+  handleTextChange,
+  value,
+}: {
+  handleTextChange: (text: string) => void
+  value: string
+}) => {
+  const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT })
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue: handleTextChange,
+  })
+  return (
+    <View>
+      <CodeField
+        ref={ref}
+        {...props}
+        value={value}
+        onChangeText={handleTextChange}
+        cellCount={CELL_COUNT}
+        keyboardType="number-pad"
+        textContentType="oneTimeCode"
+        renderCell={({ index, symbol, isFocused }) => (
+          <TextInput
+            key={index}
+            onChangeText={handleTextChange}
+            style={[styles.cell, isFocused && styles.focusCell]}
+            onLayout={getCellOnLayoutHandler(index)}>
+            {symbol || (isFocused && <Cursor />)}
+          </TextInput>
+        )}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  cell: {
+    width: 49,
+    height: 56,
+    paddingHorizontal: 16,
+    paddingVertical: 14,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginHorizontal: 8,
+    fontSize: theme.fontSize.base,
+    lineHeight: theme.lineHeight.base,
+    borderColor: theme.colors.white_200,
+    textAlign: "center",
+    color: theme.colors.white,
+  },
+  focusCell: {
+    borderColor: theme.colors.white_500,
+  },
+})
