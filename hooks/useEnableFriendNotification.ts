36310860e4ff383f30d5b05034af628d590004f5
mhenriette <mhenriette00@gmail.com>
2025-03-12 12:35:13 +0200

    fix: optimistically update the notification status

--- hooks/useEnableFriendNotification.ts
+++ hooks/useEnableFriendNotification.ts
@@ -1,14 +1,32 @@
 import AlertDialog from "@/components/AlertDialog"
 import { useSetNotificationPreferences } from "@/queries/friends"
 import { Friend } from "@/types"
+import { useQueryClient } from "@tanstack/react-query"
 
 export const useEnableFriendNotification = () => {
+  const queryClient = useQueryClient()
   const setNotificationPreferences = useSetNotificationPreferences()
   const updatePreferences = async (friend: Friend) => {
-    setNotificationPreferences.mutate({
-      enableNotification: !friend.hasNotificationEnabled,
-      friendId: friend.id,
-    })
+    const newNotificationState = !friend.hasNotificationEnabled
+    const previousFriends = queryClient.getQueryData<Friend[]>([
+      "friend-signals",
+    ])
+    queryClient.setQueryData<Friend[]>(["friend-signals"], (oldFriends = []) =>
+      oldFriends.map((oldFriend) =>
+        oldFriend.id === friend.id
+          ? { ...oldFriend, hasNotificationEnabled: newNotificationState }
+          : oldFriend,
+      ),
+    )
+    try {
+      setNotificationPreferences.mutate({
+        enableNotification: !friend.hasNotificationEnabled,
+        friendId: friend.id,
+      })
+    } catch (error) {
+      queryClient.setQueryData<Friend[]>(["friend-signals"], previousFriends)
+      console.error("Error enabling friend notification:", error)
+    }
   }
 
   const changePreferences = (friend: Friend) => {
